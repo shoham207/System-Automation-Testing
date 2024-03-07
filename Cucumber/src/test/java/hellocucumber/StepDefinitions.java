5b680f789fbdeee75a35248b393e1a14cfb96ef3
shoham207 <shohco@post.bgu.ac.il>
2024-03-07 10:59:28 +0200

    upload implementation of cucumber and selenium with two different examples

--- Cucumber/src/test/java/hellocucumber/StepDefinitions.java
+++ Cucumber/src/test/java/hellocucumber/StepDefinitions.java
@@ -2,10 +2,7 @@ package hellocucumber;
 
 import io.cucumber.java.en.*;
 
-import io.cucumber.java.After;
-import io.cucumber.java.en.*;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
+import org.junit.After;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -24,29 +21,12 @@ public class StepDefinitions {
     private ChromeDriver driver;
     private WebDriverWait wait;
     private int numberOfProductBeforeAdd;
-    private String quantityOfRemovedItem;
-    private String product = "shirt";
+    private String quantityOfProductBeforeRemoved;
+    private String quantity;
     private String userEmail = "pub@prestashop.com";
     private String userPassword = "123456789";
-    private boolean isAdminWindowOpened = false;
+    //private boolean isAdminWindowOpened = false;
     private boolean isUserWindowOpened = false;
-
-
-
-    // $$*TODO* explain what this step does$$
-    @Given("an example scenario")
-    public void anExampleScenario() {
-    }
-
-    // $$*TODO* explain what this step does$$
-    @When("all step definitions are implemented")
-    public void allStepDefinitionsAreImplemented() {
-    }
-
-    // $$*TODO* explain what this step does$$
-    @Then("the scenario passes")
-    public void theScenarioPasses() {
-    }
 
     //Auxiliary function for checking that the product has indeed been added to the wish list
     private int getNumberOfProductInWishList(){
@@ -58,7 +38,6 @@ public class StepDefinitions {
         WebElement wishlist = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"content\"]/div/ul/li/a")));
         // Retrieve the text of the element
         String Text = wishlist.getText();
-        System.out.println(Text);
         // Define a regex pattern to extract the number
         Pattern pattern = Pattern.compile("(\\d+)");
         Matcher matcher = pattern.matcher(Text);
@@ -72,6 +51,9 @@ public class StepDefinitions {
         return Integer.parseInt(numberString);
     }
 
+    // User-related steps
+
+    // Navigate to the home page
     @Given("User is in Home Page")
     public void userIsInHomePage() {
         if (!isUserWindowOpened) {
@@ -86,6 +68,7 @@ public class StepDefinitions {
         }
     }
 
+    // Log in as a user with provided credentials
     @When("User is logged in with {string} and {string}")
     public void userIsLoggedInWith(String email, String password) {
         driver.findElement(By.xpath("//body/main[1]/header[1]/nav[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/a[1]/span[1]")).click();
@@ -100,9 +83,9 @@ public class StepDefinitions {
         }
     }
 
+    // Add a product to the wish list with a specified quantity
     @And("User add a product {string} to the wishList with quantity {string}")
     public void userAddAProductToTheWishListWithQuantityX(String product, String quantity) {
-        this.product = product;
         //get the number of product before adding the product
         numberOfProductBeforeAdd = getNumberOfProductInWishList();
 
@@ -130,7 +113,7 @@ public class StepDefinitions {
     }
 
 
-
+    // Check if the product is successfully added to the user's wish list
     @Then("the product should be successfully added to the user's wishlist")
     public void theProductShouldBeSuccessfullyAddedToTheUserSWishlist() {
         int numberOfProductAfterAdd = getNumberOfProductInWishList();
@@ -138,6 +121,8 @@ public class StepDefinitions {
         //check if the product was added to the wishList
         assertEquals(numberOfProductBeforeAdd + 1, numberOfProductAfterAdd, "The product did not added to the cart");
     }
+
+    // Check if the quantity of the product in the wish list matches the expected quantity
     @And("the quantity is {string}")
     public void theQuantityIs(String quantity) {
         //check if the quantity equals to the right one
@@ -147,10 +132,9 @@ public class StepDefinitions {
         driver.findElement(By.xpath("//a[5]/span[1]")).click();
         //get into my wishList
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"content\"]/div/ul/li/a"))).click();
-        WebElement quantityDescription = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/main/section/div/div/div/section/div[1]/section/ul/li/div/a/div[2]/div/p/span[3]")));
+        WebElement quantityDescription = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"content\"]/ul/li/div/a/div[2]/div/p/span")));
         // Retrieve the text of the element
         String Text = quantityDescription.getText();
-        System.out.println(Text);
         // Define a regex pattern to extract the number
         Pattern pattern = Pattern.compile("(\\d+)");
         Matcher matcher = pattern.matcher(Text);
@@ -164,10 +148,12 @@ public class StepDefinitions {
         assertEquals(quantity, numberString, "the product's quantity is incorrect");
     }
 
+    // Admin-related steps
 
+    // Navigate to the admin home page
     @Given("Admin is in Home Page")
     public void adminIsInHomePage() {
-        if (!isAdminWindowOpened) {
+        //if (!isAdminWindowOpened) {
             System.setProperty("webdriver.chrome.driver", CHROME_DRIVER_PATH);
             this.driver = new ChromeDriver();
             this.wait = new WebDriverWait(driver, Duration.ofSeconds(40));
@@ -175,14 +161,16 @@ public class StepDefinitions {
             driver.get("http://localhost:8080/admina");
 
             driver.manage().window().maximize();
-            isAdminWindowOpened = true;
-        }
+            //isAdminWindowOpened = true;
+        //}
     }
 
+    // Log in as an admin with provided credentials
     @When("Admin is logged in with {string} and {string}")
     public void adminIsLoggedInWithAnd(String email, String password) {
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='email']"))).sendKeys(email);
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='passwd']"))).sendKeys(password);
+        driver.findElement(By.xpath("//*[@id=\"stay_logged_in\"]")).click();
         driver.findElement(By.xpath("//*[@id='submit_login']")).click();
         try {
             Thread.sleep(2000);
@@ -192,10 +180,9 @@ public class StepDefinitions {
     }
 
     private String getQuantityOfProduct(){
-        WebElement quantityDescription = driver.findElement(By.xpath("//*[@id=\"combination_list_0_delta_quantity_quantity\"]/span[1]"));
+        WebElement quantityDescription = driver.findElement(By.xpath("//*[@id=\"product_stock_quantities_delta_quantity_quantity\"]/span[1]"));
         // Retrieve the text of the element
         String Text = quantityDescription.getText();
-        System.out.println(Text);
         // Define a regex pattern to extract the number
         Pattern pattern = Pattern.compile("(\\d+)");
         Matcher matcher = pattern.matcher(Text);
@@ -209,8 +196,11 @@ public class StepDefinitions {
         return numberString;
     }
 
-    @And("admin change the quantity of the product {string} to zero")
-    public void adminChangeTheQuantityOfTheProductToZero(String product) {
+    // Change the quantity of a product to the specified quantity
+    @And("admin change the quantity of the product {string} to {string}")
+    public void adminChangeTheQuantityOfTheProductTo(String product, String quantity) {
+        this.quantity = quantity;
+
         //get into the product page
         driver.findElement(By.xpath("//*[@id=\"subtab-AdminCatalog\"]/a")).click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"subtab-AdminProducts\"]/a"))).click();
@@ -228,19 +218,19 @@ public class StepDefinitions {
         //go to the product quantity description
         driver.findElement(By.xpath("//*[@id=\"product_grid_table\"]/tbody/tr/td[9]/a")).click();
 
+        //get the current quantity for testing after
+        quantityOfProductBeforeRemoved = getQuantityOfProduct();
+
         // Locate the input field where we want to send the quantity
-        WebElement quantityInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"combination_list_0_delta_quantity_delta\"]")));
+        WebElement quantityInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"product_stock_quantities_delta_quantity_delta\"]")));
 
         // Clear the existing content of the input field
         quantityInput.clear();
-        quantityOfRemovedItem = getQuantityOfProduct();
-        String quantity = "-"+quantityOfRemovedItem; //need to subtract
 
         // Send the new quantity to the input field
         quantityInput.sendKeys(quantity);
 
         //save the changes
-        driver.findElement(By.xpath("//*[@id=\"save-combinations-edition\"]")).click();
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"product_footer_save\"]"))).click();
 
         try {
@@ -249,12 +239,13 @@ public class StepDefinitions {
             e.printStackTrace();
         }
     }
-    @Then("the quantity of the product should be zero")
-    public void theQuantityOfTheProductShouldBe() {
-        WebElement quantityDescription = driver.findElement(By.xpath("//*[@id=\"combination_list_0_delta_quantity_quantity\"]/span[1]"));
+
+    // Check if the quantity of the product in the admin panel matches the expected quantity
+    @Then("the quantity of the product should be {string}")
+    public void theQuantityOfTheProductShouldBe(String Y) {
+        WebElement quantityDescription = driver.findElement(By.xpath("//*[@id=\"product_stock_quantities_delta_quantity_quantity\"]/span[1]"));
         // Retrieve the text of the element
         String Text = quantityDescription.getText();
-        System.out.println(Text);
         // Define a regex pattern to extract the number
         Pattern pattern = Pattern.compile("(\\d+)");
         Matcher matcher = pattern.matcher(Text);
@@ -265,12 +256,13 @@ public class StepDefinitions {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        assertEquals("0",numberString,"The quantity of the product is not changed");
+        int expectedQuantity = Integer.parseInt(quantityOfProductBeforeRemoved) + Integer.parseInt(quantity);
+        assertEquals(expectedQuantity+"",numberString,"The quantity of the product is not changed");
     }
 
-
-    @And("the quantity in the user's wishlist should be adjusted accordingly")
-    public void theQuantityInTheUserSWishlistShouldBeAdjustedAccordingly() {
+    // Check if the user's wish list is adjusted accordingly after admin actions
+    @And("the user's wishlist should be adjusted accordingly for scenario {int}")
+    public void theUserSWishlistShouldBeAdjustedAccordingly(int scenarioIndex) {
         //go to the user home page
         userIsInHomePage();
         userIsLoggedInWith(userEmail,userPassword);
@@ -280,32 +272,54 @@ public class StepDefinitions {
         driver.findElement(By.xpath("//a[5]/span[1]")).click();
         //get into my wishList
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"content\"]/div/ul/li/a"))).click();
-        WebElement productDescription = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"content\"]/ul/li/div/a/div[1]/p")));
-        // Retrieve the text of the element
+        // Define the default XPath for the product description
+        String productDescriptionXPath = "//*[@id=\"content\"]/ul/li/div/a/div[1]/p";
+
+        // Check if the row index is 2 (indexing starts from 0 in Java)
+        if (scenarioIndex == 1) { // Assuming scenarioRowIndex is the index of the current scenario
+            // Modify the XPath for the second example
+            productDescriptionXPath = "//*[@id=\"content\"]/ul/li[2]/div/a/div[1]/p";
+        }
+
+        // Wait for the product description element and retrieve its text
+        WebElement productDescription = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(productDescriptionXPath)));
         String text = productDescription.getText();
-        System.out.printf(text);//debug
-        assertEquals("block\n" + "Product available with different options", text, "The user wishList is not adjusted accordingly");
+        assertEquals("block\n" + "Out-of-Stock", text, "The user wishList is not adjusted accordingly");
     }
 
-    private void removeAddedProductFromWishList() {
-        //go to the user home page
-        userIsInHomePage();
-        //userIsLoggedInWith(userEmail,userPassword);
+    // Remove added product from the wish list
+    @And("User remove all product from the wishList")
+    public void userRemoveAProductFromTheWishList() {
         //go to my account
-        driver.findElement(By.xpath("//div[2]/div[1]/a[2]/span[1]")).click();
+        //driver.findElement(By.xpath("//div[2]/div[1]/a[2]/span[1]")).click();
         //go to the wishList button
-        driver.findElement(By.xpath("//a[5]/span[1]")).click();
+        //driver.findElement(By.xpath("//a[5]/span[1]")).click();
+        int numberOfProductAfterAdd = getNumberOfProductInWishList();
+        System.out.printf(numberOfProductAfterAdd+"");
         //get into my wishList
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"content\"]/div/ul/li/a"))).click();
-
-        //press the delete button
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"content\"]/ul/li/div/div/button[2]/i"))).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"footer\"]/div[2]/div/div[1]/div[5]/div[1]/div/div/div[3]/button[2]"))).click();
-
+        for (int i=0; i<numberOfProductAfterAdd; i++) {
+            //press the delete button
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"content\"]/ul/li/div/div/button[2]/i"))).click();
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"footer\"]/div[2]/div/div[1]/div[5]/div[1]/div/div/div[3]/button[2]"))).click();
+            try {
+                Thread.sleep(6000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    @Then("the product should be successfully removed from the user's wishlist")
+    public void theProductShouldBeSuccessfullyRemovedFromTheUserSWishlist() {
+        int numberOfProductAfterRemove = getNumberOfProductInWishList();
+        assertEquals("0", numberOfProductAfterRemove+"", "The wishList isn't empty");
     }
 
-    private void addTheRemovedItemsFromAdminPage() {
+    // Add the removed items back from the admin panel
+    /*private void addTheRemovedItemsFromAdminPage() {
         adminIsInHomePage();
+        adminIsLoggedInWithAnd(adminEmail, adminPassword);
+
         //get into the product page
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"subtab-AdminCatalog\"]/a"))).click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"subtab-AdminProducts\"]/a"))).click();
@@ -320,19 +334,20 @@ public class StepDefinitions {
         productNameInput.sendKeys(product);
         productNameInput.sendKeys(Keys.ENTER);
 
+
         //go to the product quantity description
         driver.findElement(By.xpath("//*[@id=\"product_grid_table\"]/tbody/tr/td[9]/a")).click();
 
+
         // Locate the input field where we want to send the quantity
-        WebElement quantityInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"combination_list_0_delta_quantity_delta\"]")));
+        WebElement quantityInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"product_stock_quantities_delta_quantity_delta\"]")));
 
         // Clear the existing content of the input field
         quantityInput.clear();
         // Send the new quantity to the input field
-        quantityInput.sendKeys(quantityOfRemovedItem);
+        quantityInput.sendKeys(quantityOfProductBeforeRemoved);
 
         //save the changes
-        driver.findElement(By.xpath("//*[@id=\"save-combinations-edition\"]")).click();
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"product_footer_save\"]"))).click();
 
         try {
@@ -340,26 +355,26 @@ public class StepDefinitions {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-    }
+    }*/
     //@AfterEach
-    private void logoutUser() {
+    /*private void logoutUser() {
         if (driver != null) {
             driver.findElement(By.xpath("//*[@id=\"_desktop_user_info\"]/div/a[1]")).click();
         }
-    }
+    }*/
 
-
-    @AfterAll
+    // Perform cleanup actions after test execution
+    @After
     public void tearDown() {
         // Remove the added item from the wishList
-        removeAddedProductFromWishList();
+        //removeAddedProductFromWishList();
         // Add the removed items
-        addTheRemovedItemsFromAdminPage();
+        //addTheRemovedItemsFromAdminPage();
         // Close the browser session
         if (driver != null) {
             driver.quit();
         }
-        System.out.printf("here");
+
     }
 
 
